8e12fc3804c94324e27563757160d43a29bef9de
Shadan1221 <99491957+Shadan1221@users.noreply.github.com>
2025-01-30 08:47:28 +0530

    MajorityElementFinder.java

--- MajorityElementFinder.java
+++ MajorityElementFinder.java
@@ -1,17 +1,34 @@
 package Experiment3;
+import java.util.*;
 
 public class MajorityElementFinder {
     public static int majorityElement(int[] nums) {
-        int candidate = nums[0], count = 0;
-        for (int num : nums) {
-            if (count == 0) candidate = num;
-            count += (num == candidate) ? 1 : -1;
+        int n = nums.length;
+        int count = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                ans = nums[i];
+            }
+            if (ans == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
         }
-        return candidate;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println("Majority Element: " + majorityElement(nums));
+        Scanner ob = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = ob.nextInt(); 
+        int arr[] = new int[n]; 
+        System.out.println("Enter the array elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = ob.nextInt();
+        }
+        
+        System.out.println("Majority Element: " + majorityElement(arr));
+        ob.close();
     }
 }
